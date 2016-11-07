9ed6800930ec8f7ca2804b39f5b54375b2b48d84
dzieniu <dzieniu@gmail.com>
2016-11-07 22:45:42 +0100

    init

---
+++ src/matchPackages/Match.java
@@ -0,0 +1,20 @@
+
+package matchPackages;
+
+/**
+ *
+ * @author Konrad
+ */
+public class Match {
+    
+    private String matchName;
+    private double course1;
+    private double courseX;
+    private double courseY;
+    
+    public Match(){
+        
+    }
+    
+    
+}
---
+++ src/matchPackages/ReadMatches.java
@@ -0,0 +1,23 @@
+
+package matchPackages;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Konrad
+ */
+public class ReadMatches {
+    
+    ArrayList<Match> matches = new ArrayList<Match>();
+    
+    public ReadMatches(){
+    }
+    
+    public ArrayList<Match> getMatches(){
+    
+        return matches;
+    }
+    
+    
+}
